8d4d03c9937d66ff799a29aab61cd3d23e6ed839
唐卓 <tangz@yomob.com>
2018-09-03 11:47:59 +0800

    更换美术资源

---
+++ Assets/Scripts/Fish/CloudController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudController : MonoBehaviour {
+	public Transform[] clouds;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < clouds.Length; i++) {
+			clouds [i].position += new Vector3 (Random.Range (-1f, 1f), 0, 0);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < clouds.Length; i++) {
+			clouds [i].Translate (new Vector3 (speed*(i+1) * Time.deltaTime, 0, 0));
+			if (clouds [i].position.x < -9) {
+				clouds [i].position = new Vector3 (1, clouds [i].position.y, clouds [i].position.z);
+			}
+		}
+	}
+		
+}
